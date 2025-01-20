bc931ada3c1336dfccae0e22280c1d57b726e37a
promiseer <rathodparmeshwar4321@gmail.com>
2025-01-20 11:07:39 +0530

    chore: added 2 sec delay for DNMS

--- src/automations/apEcDownloader.js
+++ src/automations/apEcDownloader.js
@@ -299,6 +299,7 @@ const handleSecondForm = async (
       logger.error("Documents not found on search data.");
       return dummyFilePath;
     }
+    await delay(2000);
     await clickButton(page, "#selectAllId");
     await clickButton(page, ".btn.btn-primary");
 
@@ -494,7 +495,7 @@ const ScrapeByNone = async (
       logger.error("Documents not found on search data.");
       return dummyFilePath;
     }
-
+    await delay(2000);
     await clickButton(page, "#selectAllId");
     await clickButton(page, "button.btn.btn-primary");
     await page.waitForNavigation();
